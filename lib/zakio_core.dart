3eab8f3c411215e576f2480a1b3994490043bf26
Ahmed Magdy <ahmedmagdymohamedy@gmail.com>
2026-02-24 20:42:19 +0200

    feat: Initialize Supabase in `ZakioCore.init()` and configure Dart formatter options.

--- lib/zakio_core.dart
+++ lib/zakio_core.dart
@@ -1,5 +1,10 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class ZakioCore {
+  static void init() {
+    Supabase.initialize(
+      url: "https://fqsfcpwekbhtrybzdubb.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxc2ZjcHdla2JodHJ5YnpkdWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDgwNjMsImV4cCI6MjA4NzMyNDA2M30.CegyAJQ2Z4UY78g_Q8ikwVjXNxFLKFuIAaTFAIb4Sj4",
+    );
+  }
 }
