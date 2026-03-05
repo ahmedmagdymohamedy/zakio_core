fe7b777e0692ed975665ef4838e24b964b066c75
Ahmed Magdy <ahmedmagdymohamedy@gmail.com>
2026-03-05 03:54:36 +0200

    add auth

---
+++ lib/data/supabase/auth/auth_manager.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:zakio_core/data/supabase/supabase_manager.dart';
+
+abstract class AuthManager {
+  StreamSubscription<AuthState>? authSubscription;
+  Future<bool> signInWithOtp(String email, Function() onLogin) async {
+    try {
+      await SupabaseManager.supabase.auth.signInWithOtp(
+        email: email,
+        emailRedirectTo: "io.supabase.flutterquickstart://login-callback/",
+      );
+
+      // listen when user click on email link
+      authSubscription = SupabaseManager.supabase.auth.onAuthStateChange.listen(
+        (event) {
+          if (event.session != null) {
+            onLogin.call();
+            authSubscription?.cancel();
+          }
+        },
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
--- lib/zakio_core.dart
+++ lib/zakio_core.dart
@@ -1,10 +1,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class ZakioCore {
-  static void init() {
-    Supabase.initialize(
+  static Future<void> init() async {
+    await Supabase.initialize(
       url: "https://fqsfcpwekbhtrybzdubb.supabase.co",
       anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxc2ZjcHdla2JodHJ5YnpkdWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDgwNjMsImV4cCI6MjA4NzMyNDA2M30.CegyAJQ2Z4UY78g_Q8ikwVjXNxFLKFuIAaTFAIb4Sj4",
+      authOptions: FlutterAuthClientOptions(
+        authFlowType: AuthFlowType.pkce,
+      ),
     );
   }
 }
